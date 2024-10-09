9b15582cc023cf54007756819fa84e809e0dd96d
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-10-09 12:29:27 -0400

    a

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ function take() {
     document.querySelector("#inputx"),
     document.querySelector("#inputz"),
   ];
+
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    for (let i = 0; i < 4; i++) {
-      console.log(formdata[i - 1].value);
-    }
+    console.log(formdata[0].value, formdata[1].value, formdata[2].value);
   });
 }
 
 take();
+
+function createUserObject(name, picture, desc) {
+  return {
+    name: name,
+    picture: picture,
+    desc: desc,
+    greet: function () {
+      console.log(
+        `Hello, my name is ${this.name} and I am ${this.age} years old.`
+      );
+    },
+  };
+}
+
+createUserObject();

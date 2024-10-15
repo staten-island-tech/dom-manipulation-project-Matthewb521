bc82bcdb1c92debcf00c93d88d12ca3c5800b2c7
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-10-15 12:20:04 -0400

    a

--- index.js
+++ index.js
@@ -6,33 +6,39 @@ const DOMselectors = {
   cardContainer: document.querySelector(".card-container"),
 };
 
-function take() {
-  const form = DOMselectors.form;
+let formObject;
+
+function create() {
   const formdata = [
     document.querySelector("#inputv"),
     document.querySelector("#inputx"),
     document.querySelector("#inputz"),
   ];
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const name = formdata[0].value;
-    const picture = formdata[1].value;
-    const desc = formdata[2].value;
-    const formObject = createObject(name, picture, desc);
-    const cardHTML = `
-      <div class="card card-ani">
-        <h2>${formObject.name}</h2>
-        <img class"images" src="${formObject.picture}" alt="Image of ${formObject.name}" />
-        <h3>${formObject.desc}</h3>
-        <button class="remove-btn">Remove</button>
-      </div>
-    `;
-    DOMselectors.cardContainer.insertAdjacentHTML("beforeend", cardHTML);
-    console.log(formObject);
-    formdata.forEach((input) => (input.value = ""));
-  });
+  const name = formdata[0].value;
+  const picture = formdata[1].value;
+  const desc = formdata[2].value;
 
+  formObject = { name, picture, desc };
+}
+
+function addCard() {
+  const cardHTML = `
+  <div class="card card-ani">
+    <h2>${formObject.name}</h2>
+    <img class="images" src="${formObject.picture}" alt="Image of ${formObject.name}" />
+    <h3>${formObject.desc}</h3>
+    <button class="remove-btn">Remove</button>
+  </div>
+  `;
+
+  DOMselectors.cardContainer.insertAdjacentHTML("beforeend", cardHTML);
+  document
+    .querySelectorAll("#inputv, #inputx, #inputz")
+    .forEach((input) => (input.value = ""));
+}
+
+function removeCard() {
   DOMselectors.cardContainer.addEventListener("click", function (event) {
     if (event.target.classList.contains("remove-btn")) {
       const card = event.target.closest(".card");
@@ -41,12 +47,9 @@ function take() {
   });
 }
 
-function createObject(name, picture, desc) {
-  return {
-    name: name,
-    picture: picture,
-    desc: desc,
-  };
-}
-
-document.addEventListener("DOMContentLoaded", take);
+DOMselectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  create();
+  addCard();
+  removeCard();
+});
